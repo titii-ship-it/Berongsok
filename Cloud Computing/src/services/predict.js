c6181d14a8f671bd0684d06dbcd02777ee775d45
Abian004 <praatama@gmail.com>
2024-11-28 13:54:48 +0700

    Fixing ML recognition, using tf.node.decodeImage and normalizing the tensor

--- Cloud Computing/src/services/predict.js
+++ Cloud Computing/src/services/predict.js
@@ -1,40 +1,28 @@
 const tf = require('@tensorflow/tfjs-node');
-const sharp = require('sharp');
 
 async function predictWaste(model, imageStream) {
     try {
-        // Baca stream gambar menjadi buffer
+        // Read image stream into buffer
         const chunks = [];
         for await (const chunk of imageStream) {
             chunks.push(chunk);
         }
         const imageBuffer = Buffer.concat(chunks);
 
-        // Konversi gambar ke format PNG
-        const pngBuffer = await sharp(imageBuffer)
-            .png()
-            .toBuffer();
-
-        // Decode buffer gambar menjadi tensor
-        const tensor = tf.node
-            // .decodeImage(pngBuffer, 3)
-            // .resizeNearestNeighbor([224, 224])
-            // .expandDims()
-            // .toFloat();
-            .decodeJpeg(pngBuffer)
-            .resizeNearestNeighbor([224, 224])
+        // Decode image buffer into tensor
+        const tensor = tf.node.decodeImage(imageBuffer, 3)
+            .resizeBilinear([224, 224])
             .expandDims()
             .toFloat()
-        console.log("mengubah gambar ke tensor")
+            .div(tf.scalar(255.0));
+        console.log("Converting image to tensor");
 
-        // Melakukan prediksi
+        // Make prediction
         const prediction = model.predict(tensor);
-        const score = await prediction.data();
-        const confidenceScore = Math.max(...score) * 100;
-
-        const classResult = tf.argMax(prediction, 1).dataSync()[0];
+        const score = await prediction.array();
+        const confidenceScore = Math.max(...score[0]) * 100;
+        const classResult = score[0].indexOf(Math.max(...score[0]));
         const wasteLabel = ['Can', 'Cardboard', 'Glass Bottle', 'Paper', 'Plastic Bottle', 'Plastic Cup'];
-        // const wasteLabel = ['Plastic Cup', 'Cardboard', 'Can', 'Glass Bottle', 'Plastic Bottle', 'Paper'];
         const result = wasteLabel[classResult];
         console.log('result:', result);
 
@@ -44,7 +32,7 @@ async function predictWaste(model, imageStream) {
         };
 
     } catch (error) {
-        throw new Error(`Terjadi kesalahan dalam melakukan prediksi: ${error.message}`);
+        throw new Error(`An error occurred during prediction: ${error.message}`);
     }
 }
 
